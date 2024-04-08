1106d62c5a19238160e6faa79884005ae7eb339e
Cr4k3rhQ <103858764+ChinepohDivine-f@users.noreply.github.com>
2024-04-08 07:49:34 +0100

    created user object with and override method
    serelization: moving from dart to json
    we can do this using Maps or a custom method as seen  in the code

--- chapter6.dart
+++ chapter6.dart
@@ -3,6 +3,13 @@ import 'package:chapter6/chapter6.dart' as chapter6;
 void main() {
   final object = MyClass();
   object.myMethod();
+
+  // create and instace of user
+  final user = User();
+  user.id = 42;
+  user.name = 'Boris Atoh';
+  print(user.toJson());
+  print(user.toString());
 }
 
 class MyClass {
@@ -14,5 +21,23 @@ class MyClass {
   // method
   void myMethod() {
     print(this.myPorperty);
+  }
+}
+
+// creating a sample user class
+class User {
+  int id = 0;
+  String name = '';
+
+  //ovride
+  @override
+  String toString() {
+    return 'User(id: $id, name: $name)';
+  }
+
+  // sereliztion
+  //  to json
+  String toJson() {
+    return '{"id": $id, "name":"$name"}';
   }
 }
